0c997f0f6d2366ebbee00ddc79fe92415ddc2c46
Gal PC <glpkmtg@gmail.com>
2019-01-04 12:55:59 +0200

    FollowedPersons table

--- src/MovieDB/API.hs
+++ src/MovieDB/API.hs
@@ -1,4 +1,5 @@
 module MovieDB.API(
+  ApiCall,
   I.ApiKey(..),
   I.ApiQuery,
   runQuery,
@@ -8,6 +9,7 @@ module MovieDB.API(
   personCredits,
 ) where
 
+import           MovieDB.API.Internal       (ApiCall)
 import qualified MovieDB.API.Internal       as I
 
 import           Common.JsonObjectParser    (parseObject)
@@ -20,27 +22,26 @@ import           Control.Monad.Trans.Reader (ReaderT, ask)
 import qualified Data.List.NonEmpty         as NEL (fromList)
 import           Data.Text                  (unpack)
 
-import           MovieDB.Types              (CastAndCrew(..), Movie(..),
-                                             Participation(..), ParticipationType, Person(..),
-                                             toCastAndCrew)
+import           MovieDB.Types              (CastAndCrew(..), Movie(..), Participation(..), ParticipationType,
+                                             Person(..), toCastAndCrew)
 
 import           Network.HTTP               (getRequest, getResponseBody, simpleHTTP)
 
-runQuery :: I.ApiQuery q r => q -> I.ApiCall r
+runQuery :: I.ApiQuery q r => q -> ApiCall r
 runQuery q = do
   request <- ask <$$> I.apiPath q <$$> unpack
   let res = simpleHTTP (getRequest request) >>= getResponseBody
   json <- liftIO $ res <$$> decodeUnsafe
   return $ fromSuccess $ parseObject I.parse json
 
-castAndCrew :: Movie -> I.ApiCall CastAndCrew
+castAndCrew :: Movie -> ApiCall CastAndCrew
 castAndCrew m = do
   ps <- participations m <$> runQuery (I.forMovie m)
   return $ toCastAndCrew $ NEL.fromList ps where
     participations :: Movie -> [(Person, ParticipationType)] -> [Participation]
     participations movie = map aux where aux (p, pt) = Participation p movie pt
 
-personCredits :: Person -> I.ApiCall [Participation]
+personCredits :: Person -> ApiCall [Participation]
 personCredits p = do
   ps <- runQuery $ I.forPerson p
   return $ map participations ps where
--- src/MovieDB/Database/FollowedPersons.hs
+++ src/MovieDB/Database/FollowedPersons.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE FlexibleContexts, GADTs, GeneralizedNewtypeDeriving, MultiParamTypeClasses, QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell, TypeFamilies                                                           #-}
 
-module MovieDB.Database.SeenMovies(
+module MovieDB.Database.FollowedPersons(
   init,
   clear,
-  addSeenMovie,
-  removeSeenMovie,
+  addFollowedPerson,
+  removeFollowedPerson,
 ) where
 
 import Prelude                    hiding (init)
@@ -13,7 +13,7 @@ import Prelude                    hiding (init)
 import Common.Operators
 
 import MovieDB.Database.Common    (DbCall, path)
-import MovieDB.Database.Movies    (MovieRowId, MovieRowable, toMovieRowId)
+import MovieDB.Database.Persons   (PersonRowId, PersonRowable, toPersonRowId)
 
 import Control.Monad              ((>=>))
 import Control.Monad.Trans.Reader (ask)
@@ -23,9 +23,9 @@ import Database.Persist.Sqlite    (runMigrationSilent, runSqlite)
 import Database.Persist.TH        (mkMigrate, mkPersist, persistLowerCase, share, sqlSettings)
 
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
-SeenMovies
-  movieId         MovieRowId
-  UniqueMovieId   movieId
+FollowedPersons
+  personId        PersonRowId
+  UniquePersonId  personId
 |]
 
 withMigration action = do
@@ -37,10 +37,10 @@ init :: DbCall()
 init = withMigration $ return ()
 
 clear :: DbCall()
-clear = withMigration $ deleteWhere ([] :: [Filter SeenMovies])
+clear = withMigration $ deleteWhere ([] :: [Filter FollowedPersons])
 
-addSeenMovie :: MovieRowable m => m -> DbCall SeenMoviesId
-addSeenMovie = toMovieRowId >=> (withMigration . insert . SeenMovies)
+addFollowedPerson :: PersonRowable m => m -> DbCall FollowedPersonsId
+addFollowedPerson = toPersonRowId >=> (withMigration . insert . FollowedPersons)
 
-removeSeenMovie :: MovieRowable m => m -> DbCall ()
-removeSeenMovie = toMovieRowId >=> (withMigration . deleteBy . UniqueMovieId)
+removeFollowedPerson :: PersonRowable m => m -> DbCall ()
+removeFollowedPerson = toPersonRowId >=> (withMigration . deleteBy . UniquePersonId)
