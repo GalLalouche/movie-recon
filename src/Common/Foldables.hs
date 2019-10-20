526bf9f17da600b8b96a2bf1506bd27e54b8292f
Gal PC <glpkmtg@gmail.com>
2019-10-20 17:00:12 +0300

    Foldables.mapHeadOrElse

--- src/Common/Foldables.hs
+++ src/Common/Foldables.hs
@@ -1,7 +1,7 @@
 module Common.Foldables where
 
 import Data.Foldable (null, toList)
-import Data.Maybe (fromJust)
+import Data.Maybe    (fromJust)
 
 
 notNull :: Foldable f => f a -> Bool
@@ -15,6 +15,11 @@ headUnsafe = fromJust . headOpt
 
 nth :: Foldable f => Int -> f a -> Maybe a
 nth i = aux i . toList where
-  aux _ [] = Nothing
+  aux _ []     = Nothing
   aux 0 (x:xs) = Just x
   aux n (_:xs) = aux (n - 1) xs
+
+mapHeadOrElse :: Foldable f => (a -> b) -> b -> f a -> b
+mapHeadOrElse f def foldable = case headOpt foldable of
+  Nothing  -> def
+  (Just x) -> f x
