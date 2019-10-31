bde7bb6ef82c6c19a69c4e73b45f353db9c17c14
Gal PC <glpkmtg@gmail.com>
2019-10-31 22:46:38 +0200

    UTF8 encoding

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,10 @@ import           Control.Monad.Trans.Reader (runReaderT)
 
 import           MovieDB.Database           (DbCall, DbPath(..), runDbCall)
 
-import qualified Main.Action as Action
-import qualified Main.Config as Config
+import           GHC.IO.Encoding            (setLocaleEncoding, utf8)
+
+import qualified Main.Action                as Action
+import qualified Main.Config                as Config
 
 
 dbPath = DbPath "db.sqlite"
@@ -17,11 +19,12 @@ withBoth :: Action.JoinedAction -> IO ()
 withBoth = flip runReaderT dbPath
 
 main = do
+  setLocaleEncoding utf8
   args <- Config.parseConfig
   case args of
-    Config.Init                             -> withDb Action.initDatabases
-    (Config.GetUnseen verbose)              -> withDb $ Action.printUnseenMovies verbose
-    Config.UpdateSeen                       -> withDb Action.parseSeenMovies
-    Config.UpdateIndex                      -> withBoth Action.updateMoviesForAllFollowedPersons
-    Config.UpdateScores                     -> withBoth Action.updateScores
-    (Config.AddPerson url ignoreActing)     -> withBoth $ Action.addFollowedPerson url ignoreActing
+    Config.Init                         -> withDb Action.initDatabases
+    (Config.GetUnseen verbose)          -> withDb $ Action.printUnseenMovies verbose
+    Config.UpdateSeen                   -> withDb Action.parseSeenMovies
+    Config.UpdateIndex                  -> withBoth Action.updateMoviesForAllFollowedPersons
+    Config.UpdateScores                 -> withBoth Action.updateScores
+    (Config.AddPerson url ignoreActing) -> withBoth $ Action.addFollowedPerson url ignoreActing
